45335a8e49e44f179d1884d74a31dd62ca7c4bad
Николай Кочкин <urfiner@gmail.com>
2024-01-21 22:26:55 +0100

    Initial commit

---
+++ PandocWrapper.php
@@ -0,0 +1,134 @@
+<?php
+use MediaWiki\Shell\Shell;
+use MediaWiki\MediaWikiServices;
+use MediaWiki\Title\Title;
+
+function findFiles ($dir, &$results = array()) {
+    $files = scandir($dir);
+
+    foreach ($files as $key => $value) {
+        $path = realpath($dir . DIRECTORY_SEPARATOR . $value);
+        if (!is_dir($path)) {
+            $results[] = $path;
+        } else if ($value != "." && $value != "..") {
+            findFiles($path, $results);
+            $results[] = $path;
+        }
+    }
+
+    return $results;
+}
+
+class PandocWrapper{
+    private static $supportedFormats = [
+        "docx" => "docx",
+        "md" => "markdown",
+        "markdown" => "markdown"
+    ];
+
+
+    public static function convert($filePath){
+        global $wgPandocExecutablePath;
+        global $wgPandocTmpFolderPath;
+
+        $ext = pathinfo($filePath, PATHINFO_EXTENSION);
+        $base_name = pathinfo($filePath, PATHINFO_FILENAME);
+        $subfolder_name = join(DIRECTORY_SEPARATOR, [$wgPandocTmpFolderPath, pathinfo($filePath, PATHINFO_FILENAME)]);
+        $subfolder_name = str_replace(array('/', '\\'), DIRECTORY_SEPARATOR, $subfolder_name);
+
+        // Try to upload even if format is not in the list
+        // In the future we may want to extend list of formats.
+        $sourceFormat = self::$supportedFormats[$ext] ?? $ext;
+		$res = Shell::command(
+			$wgPandocExecutablePath,
+			'--from=' . $sourceFormat,
+			'--to=mediawiki',
+            '--extract-media='. $subfolder_name,
+			$filePath
+		  )->includeStderr()
+			->execute();
+
+        //Return text part and path to folder
+        return [
+            "text" => $res->getStdout(),
+            "baseName" =>  $base_name,
+            "mediaFolder" => $subfolder_name
+        ];
+    }
+
+    public static function processImages($subfolder_name, $base_name, $user){
+        $imagesVocabulary = [];
+
+        $services = MediaWikiServices::getInstance();
+
+        $files = findFiles( $subfolder_name );
+        foreach ($files as $file){
+            $base = wfBaseName( $file );
+            $file_page_name = $base_name . '-' . $base;
+            $title = Title::makeTitleSafe( NS_FILE, $file_page_name );
+            $image = $services->getRepoGroup()->getLocalRepo()
+                ->newFile( $title );
+            
+            $sha1 = FSFile::getSha1Base36FromPath( $file );
+            
+            # check duplicates, skip upload duplicate (use duplicate)
+            # TODO - make it an optioal parameter?
+            $repo = $image->getRepo();
+            $dupes = $repo->findBySha1( $sha1 );
+            if ( $dupes ) {
+                $imagesVocabulary[$file] = $dupes[0]->getName();
+                continue; 
+            }
+
+            # Upload missing file
+            $flags = 0;
+            $publishOptions = [];
+            $archive = $image->publish( $file, $flags, $publishOptions );
+            if ( !$archive->isGood() ) {
+                $errorMesg = $archive->getMessage( false, false, 'en' )->text() . "\n";
+                throw new Exception ($errorMesg );
+            }
+            $mwProps = new MWFileProps( $services->getMimeAnalyzer() );
+            if ($image->recordUpload3(
+                $archive->value,
+                wfMessage("pandocultimateconverter-history-comment"), 
+                '',
+                $user,
+                $mwProps->getPropsFromPath( $file, true )
+            )->isOK()){
+                $imagesVocabulary[$file] = $file_page_name;
+                continue;
+            }
+            else{
+                throw new Exception('Failed to upload ' . $file);
+            }
+            
+        }
+
+        return $imagesVocabulary;
+    }
+
+    public static function deleteDirectory($dir) {
+        if (!file_exists($dir)) {
+            return true;
+        }
+    
+        if (!is_dir($dir)) {
+            return unlink($dir);
+        }
+    
+        foreach (scandir($dir) as $item) {
+            if ($item == '.' || $item == '..') {
+                continue;
+            }
+    
+            if (!self::deleteDirectory($dir . DIRECTORY_SEPARATOR . $item)) {
+                return false;
+            }
+    
+        }
+    
+        return rmdir($dir);
+    }
+
+}
---
+++ modules/ext.pandocUltimateConverterSpecial.js
@@ -0,0 +1,73 @@
+﻿$(document).ready(function() {
+  function uuidv4() {
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+  }
+  function getExtension(filename) {
+    return filename.split('.').pop();
+  }
+
+  var page_name_MIN_LENGTH = 4;
+  var page_name_MAX_LENGTH = 255;
+
+  var page_name_field = $("#wpArticleTitle");
+  var upload_file_field = $("#wpUploadFile");
+  var hidden_file_name = $("#wpUploadedFileName");
+  var submit_form = $("#mw-pandoc-upload-form");
+  var submit_button = $("#mw-pandoc-upload-form-submit");
+
+  var invalid_regex = /[\/\'\"\$]/g;
+  if (submit_form.length == 1) {
+    submit_form.on('submit', function (event) {
+      event.preventDefault();
+
+      var results;
+      var page_name = page_name_field.val()
+
+      // Validate page name
+      if (page_name.length < page_name_MIN_LENGTH || page_name.length > page_name_MAX_LENGTH) {
+        alert(mw.message('pandocultimateconverter-warning-page-name-length').text().replace("$1", page_name_MIN_LENGTH).replace("$2", page_name_MAX_LENGTH));
+        return false;
+      }
+
+      if ((results = page_name.match(invalid_regex)) != null) {
+        results = results.join(" ");
+        alert(mw.message('pandocultimateconverter-warning-page-name-invalid-character').text().replace("$1", results));
+        return false;
+      }
+
+      // Validate file selector
+      if (!upload_file_field.val()){
+        alert(mw.message('pandocultimateconverter-warning-file-not-selected').text())
+      }
+
+      // Upload image
+      ext = getExtension(upload_file_field.val()).toLowerCase();
+      file_name = 'pandocultimateconverter-' + uuidv4() + "." + ext;
+      hidden_file_name.val(file_name);
+      
+      submit_form.append('<div style="" id="loadingDiv"><div class="loader">Loading...</div></div>');
+      api = new mw.Api();
+      var upload_file_params = {
+        format: 'json',
+        stash: false,
+        ignorewarnings: 1,
+        filename: file_name
+      };  
+
+
+      // TODO: add waiting form
+      api.upload(upload_file_field[0], upload_file_params).fail(data =>{
+      }).always( data => {
+        //go to backend logic after that
+        $( "#loadingDiv" ).fadeOut(500, function() {
+          $( "#loadingDiv" ).remove(); //makes page more lightweight 
+        });
+        this.submit();
+      } );
+      
+    });
+  }
+
+});
